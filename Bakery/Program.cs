769aacc3d94c7128746213f4821ce43a1dbe0e0c
Ryan Ashby <rjapsu160@gmail.com>
2022-02-25 08:42:48 -0800

    add basic frontend prompts for user

---
+++ Bakery/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bakery.Models;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pete's Bakery");
+      Console.WriteLine("Your Home for All Your Favorite Bread and Pastry");
+      Console.WriteLine("How many loaves of bread would you like to purchase today?");
+      Console.ReadLine();
+      Console.WriteLine("How many pastries would you like to purchase today?");
+      Console.ReadLine();
+    }
+  }
+}
