0b0d18e8b70957e5eeb1fa1e1f3ba62b4951cc9d
Ryan Ashby <rjapsu160@gmail.com>
2022-02-25 14:31:49 -0800

    finished front end. application up and running

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,12 +9,17 @@ namespace Bakery
   {
     public static void Main()
     {
+      Bread bread = new Bread(0, 0);
+      Pastry pastry = new Pastry(0, 0);
       Console.WriteLine("Welcome to Pete's Bakery");
       Console.WriteLine("Your Home for All Your Favorite Bread and Pastry");
       Console.WriteLine("How many loaves of bread would you like to purchase today?");
-      Console.ReadLine();
+      int breadOrder = Convert.ToInt32(Console.ReadLine());
       Console.WriteLine("How many pastries would you like to purchase today?");
-      Console.ReadLine();
+      int pastryOrder = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine($"The total cost of your bread is {bread.BreadAmount(breadOrder)}");
+      Console.WriteLine($"And your pastry total is {pastry.PastryAmount(pastryOrder)}");
+      Console.WriteLine($"Your grand total is {bread.BreadAmount(breadOrder) + pastry.PastryAmount(pastryOrder)}");
     }
   }
 }
