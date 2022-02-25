4198bfafdc71e693fedea7aaae3e869abc372c4c
Ryan Ashby <rjapsu160@gmail.com>
2022-02-25 08:18:24 -0800

    finish initial framework/dotnet restore/boilerplate for basic files

---
+++ Bakery.Tests/ModelTests/BakeryItemsTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScrabbleScorer.Tests 
+{
+  [TestClass]
+  public class CalculateTests
+  {
+    [TestMethod]
+    public void XXXX()
+    {
+      
+    }
+    
+  }
+}
---
+++ Bakery/Models/BakeryItems.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Bakery.Models
+{
+  public class Bread
+  {
+    
+  }
+  public class Pastry
+  {
+    
+  }
+}
