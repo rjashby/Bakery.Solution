22e027134471352f88edbcddd87db21ba229dc0e
Ryan Ashby <rjapsu160@gmail.com>
2022-02-25 14:40:02 -0800

    add more console statements, explaining pricing to user

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -13,13 +13,15 @@ namespace Bakery
       Pastry pastry = new Pastry(0, 0);
       Console.WriteLine("Welcome to Pete's Bakery");
       Console.WriteLine("Your Home for All Your Favorite Bread and Pastry");
+      Console.WriteLine("Bread Prices: \n A single loaf costs $5.00. \n But you can buy 2, and get the third free!");
+      Console.WriteLine("Pastry Prices: \n A single pastry costs $2.00. \n But for every two pastries you buy, your next pastry is only $1.00!");
       Console.WriteLine("How many loaves of bread would you like to purchase today?");
       int breadOrder = Convert.ToInt32(Console.ReadLine());
       Console.WriteLine("How many pastries would you like to purchase today?");
       int pastryOrder = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine($"The total cost of your bread is {bread.BreadAmount(breadOrder)}");
-      Console.WriteLine($"And your pastry total is {pastry.PastryAmount(pastryOrder)}");
-      Console.WriteLine($"Your grand total is {bread.BreadAmount(breadOrder) + pastry.PastryAmount(pastryOrder)}");
+      Console.WriteLine($"The total cost for your bread is ${bread.BreadAmount(breadOrder)} dollars.");
+      Console.WriteLine($"And your pastry total is ${pastry.PastryAmount(pastryOrder)} dollars.");
+      Console.WriteLine($"Your grand total is ${bread.BreadAmount(breadOrder) + pastry.PastryAmount(pastryOrder)} dollars.");
     }
   }
 }
